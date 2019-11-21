9216335a84fd1e86af24296a485168762caee919
kashyapabhishek <kashyap638@gmail.com>
2019-11-21 14:07:58 +0530

    Enable the both video and audio option

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,52 +1,54 @@
 "use strict";
 
+// ------------------------------Extract the query string from URL or iframe ----------------------------------//
+
 if(location.href.includes('?')){
   let params =  location.href.split('?')[1].split('=')[1];
-  sessionStorage.setItem('localusername',params);
+  sessionStorage.setItem('localusername', params);
 }
-let videos = false;
-let audios = true;
 
-//------------------------ WebRT single-peer End-------------------------------//
-
- ////////////////// make singalR connection ////////////////////////////////
+ // Make singalR connection object
  var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
- /////////////////// Start the signalR services ////////////////////////////
+ // Start the signalR Hub connetion
 connection.start().then(function () {
 
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
+
+// Get the borwser media for Chrome, firefox, IE
 navigator.getUserMedia = (navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia);
   
-  navigator.getUserMedia({ video: videos, audio: audios },gotMedia, () => {});
+// Gate the borwser media with video and audio capacity 
+navigator.getUserMedia({ video: true, audio: true },gotMedia, () => {});
 
 
-  async function gotMedia(stream){
-      
+// function to take MediaStream and with SimplePeer Make P2P connection.
+async function gotMedia(stream){
+
+  // create a SimplePeer object (peer) with initiator URL start with https://<url:port>?#init
   var peer = new SimplePeer({
     initiator: location.hash == '#init',
     trickle: false,
     stream: stream
     });
 
+    // Generate the Networkin information for the data transmission 
     peer.on('signal', function (data) {
             connection.invoke("SendMessage", sessionStorage.getItem('localusername'), sessionStorage.getItem('callerusername'),  JSON.stringify(data)).catch(function (err) {
                 return console.error(err.toString());
              });
-
-
-     
     });
 
+    // Get stream and push the video content on the web page in vido tage 
     peer.on('stream', function (stream) {
-      document.getElementById('tempvideo').remove();
-        var video = document.createElement('video')
+      document.getElementById('tempvideo').remove(); // remove the existing video tag
+        var video = document.createElement('video') // create new video tag
         video.setAttribute("style", "width: 100% !important");
         video.controls = true;
         video.poster = "~/images/callingimage.png";
@@ -54,6 +56,8 @@ navigator.getUserMedia = (navigator.getUserMedia ||
         video.srcObject = stream
         video.play();
     })
+
+    // signal r connection On ReceiveMessage set the local username , callerusername and call the signal to create the networking information for the othe rought
     connection.on("ReceiveMessage", function (caller,reciver, message) {
         console.log("recievedMessage"+reciver+"::"+caller);
           var orderid = JSON.parse(message)
@@ -65,12 +69,7 @@ navigator.getUserMedia = (navigator.getUserMedia ||
   
   }
 
-   ////////////////////// Recieve The message  ///////////////////////////////
-
-
-
-  ////////////////////  make call or send message ////////////////////////
-
+// Simple function for prompt to ask reciver id
 function myFunction() {
     var person = prompt("Please Enter reciver id");
     if (person != null) {
@@ -79,43 +78,27 @@ function myFunction() {
     return null;
   }
   
-  document.getElementById("makevideocall").addEventListener("click", function (event) {
-    var video = true;
-    var audio = true;
+// Function to take reciver id and set the callerusername
+  function call(){
     var returnMsg = myFunction();
     if(returnMsg != null)
-    {
-      sessionStorage.setItem("caller","yes");        
+    {       
       sessionStorage.setItem("callerusername",returnMsg);
       location.href = '/#init';
       //location.reload();
     }else{
       alert("Enter the reciver id");
     }
+  }
+
+  // work click event for makevideo call 
+  document.getElementById("makevideocall").addEventListener("click", function (event) {
+    call();
     event.preventDefault();
   });
   
+  // click event for makeaudio call  -- (this is not emplimented right now)
   document.getElementById('makecall').addEventListener('click', function(event){
-      audios = true;
-      var returnMsg = myFunction();
-    if(returnMsg != null)
-    {
-      sessionStorage.setItem("caller","yes");        
-      sessionStorage.setItem("callerusername",returnMsg);
-      location.href = '/#init';
-      //location.reload();
-    }else{
-      alert("Enter the reciver id");
-    }
+      call();
     event.preventDefault();
   });
-
-  
-
-// document.getElementById('loginbutton').addEventListener('click', function(){
-//    var userid =  myFunction();
-//    sessionStorage.setItem("localusername", userid);
-//    document.getElementById('user').innerHTML = userid;
-
-// })
-  
